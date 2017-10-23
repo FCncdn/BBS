cdfe5f6e63e05848f4183d4a819b52dde9adfeef
qq468458093 <15flfeng@stu.edu.cn>
2017-10-23 22:15:47 +0800

    Revert "first_version"
    This reverts commit a1e11192e3dbcf3ded20357e97a4fba762146c01.

--- STU_BBS/wsgi.py
+++ STU_BBS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BBSPRO project.
+WSGI config for STU_BBS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BBSPRO.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "STU_BBS.settings")
 
 application = get_wsgi_application()
